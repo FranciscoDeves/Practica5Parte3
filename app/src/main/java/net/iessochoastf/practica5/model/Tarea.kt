0399d9ace3e0ae5d8bf3c40b4ebcec56fd963f9d
FranciscoDeves <fradevrui@alu.edu.gva.es>
2023-11-26 16:40:20 +0100

    Punto 10 Añadido ModelTempTareas

---
+++ app/src/main/java/net/iessochoastf/practica5/model/Tarea.kt
@@ -0,0 +1,43 @@
+package net.iessochoastf.practica5.model
+
+data class Tarea(
+
+    var id:Long?=null,//id único
+    val categoria:Int,
+    val prioridad:Int,
+    val pagado:Boolean,
+    val estado:Int,
+    val horasTrabajo:Int,
+    val valoracionCliente:Float,
+    val tecnico:String,
+    val descripcion:String
+
+
+)
+
+{
+
+    //segundo constructor que genera id nuevo
+
+    constructor( categoria:Int,
+                 prioridad:Int,
+                 pagado:Boolean,
+                 estado:Int,
+                 horasTrabajo:Int,
+                 valoracionCliente:Float,
+                 tecnico:String,
+                 descripcion:String):this(generateId(),categoria,prioridad,pagado,estado,horasTrabajo,valoracionCliente, tecnico, descripcion){}
+
+    companion object {
+        var idContador = 1L//iniciamos contador de tareas
+        private fun generateId(): Long {
+            return idContador++//sumamos uno al contador
+        }
+    }
+        //dos tareas son iguales cuando su id es igual.
+        // Facilita la búsqueda en un arrayList
+        override fun equals(other: Any?): Boolean {
+            return (other is Tarea)&&(this.id == other?.id)
+        }
+
+}
